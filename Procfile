deb25b8b669d3ccd6dc53eaf38b620d0ff41d355
sallyjayz <salama.jatau@yahoo.com>
2021-01-28 16:41:12 +0100

    added settings for deployment

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --pythonpath mysite mysite.wsgi --log-file -
+web: gunicorn --pythonpath mysite mysite.deploy --log-file -
---
+++ mysite/deploy_settings/__init__.py
@@ -0,0 +1,9 @@
+from mysite.mysite.settings import *
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+ALLOWED_HOSTS = [
+    'localhost',
+    '.herokuapp.com',
+]
