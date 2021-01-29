099497354b51fa534b8e2f7a0add50c558965f54
sallyjayz <salama.jatau@yahoo.com>
2021-01-29 10:26:46 +0100

    add whitenoise to project updated

--- mysite/deploy.py
+++ mysite/deploy.py
@@ -8,10 +8,8 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-from whitenoise import WhiteNoise
-
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.deploy_settings')
 
